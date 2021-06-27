a3d6e3fbf4399ddb42bebcceb0f81921af41618e
akio0911 <akio0911@gmail.com>
2021-06-28 07:45:44 +0900

    feedback

--- Loco.Kadai7/RedViewController.swift
+++ Loco.Kadai7/RedViewController.swift
@@ -1,11 +1,7 @@
 import UIKit
 
-// デリゲートを使うためプロトコル準備。
-protocol CalculateProtocol: AnyObject {
-    func calculateAndDisplay(sing: (Int, Int) -> Int, firstTextField: UITextField, secondTextField: UITextField, calculateNumberLabel: UILabel)
-}
 // 計算式
-extension CalculateProtocol {
+struct Calculator {
 // 引数で、算術記号を決めれるようにした。
     func calculateAndDisplay(sing: (Int, Int) -> Int, firstTextField: UITextField, secondTextField: UITextField, calculateNumberLabel: UILabel) {
         let firstValue = Int(firstTextField.text!) ?? 0
@@ -14,13 +10,14 @@ extension CalculateProtocol {
         calculateNumberLabel.text! = String(sing(firstValue, secondValue))
     }
 }
-class RedViewController: UIViewController, CalculateProtocol {
+
+class RedViewController: UIViewController {
     @IBOutlet weak private var firstTextField: UITextField!
     @IBOutlet weak private var secondTextField: UITextField!
     @IBOutlet weak private var calculateNumberLabel: UILabel!
 
     @IBAction private func calculateButton(_ sender: Any) {
-        calculateAndDisplay(sing: (+), firstTextField: firstTextField, secondTextField: secondTextField, calculateNumberLabel: calculateNumberLabel)
+        Calculator().calculateAndDisplay(sing: +, firstTextField: firstTextField, secondTextField: secondTextField, calculateNumberLabel: calculateNumberLabel)
 
         view.endEditing(true)
     }
