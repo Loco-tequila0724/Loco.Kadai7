d72507a50be920004f1e7d3cfbb69430d9a612fa
Loco-tequila0724 <hayato09097699090@icloud.com>
2021-06-28 08:48:04 +0900

    課題７　フィードバック済

--- Loco.Kadai7/GreenViewController.swift
+++ Loco.Kadai7/GreenViewController.swift
@@ -4,17 +4,10 @@ class GreenViewController: UIViewController {
     @IBOutlet weak private var firstTextField: UITextField!
     @IBOutlet weak private var secondTextField: UITextField!
     @IBOutlet weak private var calculateNumberLabel: UILabel!
-    private weak var delegate: CalculateProtocol?
-    private let redController = RedViewController()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.delegate = redController
-    }
+    private let calculator = Calculator()
 
     @IBAction private func calculateButton(_ sender: Any) {
-        self.delegate?.calculateAndDisplay(sing: (-), firstTextField: firstTextField, secondTextField: secondTextField, calculateNumberLabel: calculateNumberLabel)
-
+        calculator.calculateAndDisplay(sing: -, firstTextField: firstTextField, secondTextField: secondTextField, calculateNumberLabel: calculateNumberLabel)
         view.endEditing(true)
     }
 }
--- Loco.Kadai7/RedViewController.swift
+++ Loco.Kadai7/RedViewController.swift
@@ -1,12 +1,6 @@
 import UIKit
 
-// デリゲートを使うためプロトコル準備。
-protocol CalculateProtocol: AnyObject {
-    func calculateAndDisplay(sing: (Int, Int) -> Int, firstTextField: UITextField, secondTextField: UITextField, calculateNumberLabel: UILabel)
-}
-// 計算式
-extension CalculateProtocol {
-// 引数で、算術記号を決めれるようにした。
+struct Calculator {
     func calculateAndDisplay(sing: (Int, Int) -> Int, firstTextField: UITextField, secondTextField: UITextField, calculateNumberLabel: UILabel) {
         let firstValue = Int(firstTextField.text!) ?? 0
         let secondValue = Int(secondTextField.text!) ?? 0
@@ -14,13 +8,14 @@ extension CalculateProtocol {
         calculateNumberLabel.text! = String(sing(firstValue, secondValue))
     }
 }
-class RedViewController: UIViewController, CalculateProtocol {
+class RedViewController: UIViewController {
     @IBOutlet weak private var firstTextField: UITextField!
     @IBOutlet weak private var secondTextField: UITextField!
     @IBOutlet weak private var calculateNumberLabel: UILabel!
+    private let calculator = Calculator()
 
     @IBAction private func calculateButton(_ sender: Any) {
-        calculateAndDisplay(sing: (+), firstTextField: firstTextField, secondTextField: secondTextField, calculateNumberLabel: calculateNumberLabel)
+        calculator.calculateAndDisplay(sing: +, firstTextField: firstTextField, secondTextField: secondTextField, calculateNumberLabel: calculateNumberLabel)
 
         view.endEditing(true)
     }
